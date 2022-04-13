4549043ce8b74d8bebca23c6406d54a7e4162e73
Hunter Fox <hunterjfox6@gmail.com>
2022-04-13 15:20:08 -0500

    Mobile menu icon change

--- js/app.js
+++ js/app.js
@@ -4,5 +4,3 @@
 
 $(document).foundation();
 
-
-//Accordions
--- js/shield.js
+++ js/shield.js
@@ -4,5 +4,20 @@
 
 $(document).foundation();
 
+;//mobile menu icon
 
-//Accordions
+const menuIcon = document.querySelector(".menu-icon"); //Select menu icon 'button'
+
+menuIcon.addEventListener("click", changeIcon); //listen for click
+
+
+function changeIcon(){
+
+    if ($(menuIcon).hasClass('menu-open')) { //if menu is already open...
+
+        $(menuIcon).removeClass('menu-open'); //remove class that adds X icon, defaults back to menu icon
+    } else {
+        $(menuIcon).addClass('menu-open'); //if menu is closed, add class that replaces icon
+    }
+
+}
